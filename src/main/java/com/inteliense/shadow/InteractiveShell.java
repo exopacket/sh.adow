abf93ede5a45a268d60d6a394f3ebea208375687
Ryan Fitzgerald <ryan@inteliense.com>
2023-01-28 18:42:36 -0500

    ...

--- src/main/java/com/inteliense/shadow/InteractiveShell.java
+++ src/main/java/com/inteliense/shadow/InteractiveShell.java
@@ -9,6 +9,8 @@ public class InteractiveShell {
 
     private static Scanner scnr = new Scanner(System.in);
 
+    private static ArrayList<String> history = new ArrayList<String>();
+
     public static void main() {
 
         String pwd = "";
@@ -19,7 +21,16 @@ public class InteractiveShell {
         while(!exit) {
 
             //TODO change nextLine() to next() for grabbing arrows, delete, enter, etc.
-            //TODO add custom functions
+            //TODO add custom functions (ex: 'edit' to open default text editor for saving the contents)
+
+            //edit [filename]: opens the file in your editor & saves changes for use on another machine
+            //ignore [command]: ignores all of a specific command (prompts for session or global use)
+            //forget: forgets the current terminal session
+            //pause: pauses collecting terminal commands
+            //last: shows last command
+            //last-[n]: shows last [n] commands
+            //continue: continues collecting commands
+            //exit: exit's the interactive shell
 
             System.out.print("/bin/sh (collecting) $ ");
             String input = scnr.nextLine();
@@ -34,6 +45,9 @@ public class InteractiveShell {
 
     }
 
+    private static String[] getCommand() {
+        return null;
+    }
 
     private static String fixPath(String in) {
         if(!in.endsWith("/")) return in + "/";
